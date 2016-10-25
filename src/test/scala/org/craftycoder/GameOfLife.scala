8b50ef2b34e4be8b14860875b0c5643554130c95
Carlos <carlospb86@gmail.com>
2016-10-25 20:32:48 +0100

    Refactor: Add isAlive parameter in order to ready for the upcoming business rules

--- src/test/scala/org/craftycoder/GameOfLife.scala
+++ src/test/scala/org/craftycoder/GameOfLife.scala
@@ -4,19 +4,19 @@ import org.scalatest.{Matchers, WordSpecLike}
 
 class GameOfLife extends WordSpecLike with Matchers {
 
-  def isAliveNextGen(numberOfNeighboursAlive: Int): Boolean = numberOfNeighboursAlive == 2 || numberOfNeighboursAlive == 3
+  def isAliveNextGen(numberOfNeighboursAlive: Int, isAlive: Boolean): Boolean = numberOfNeighboursAlive == 2 || numberOfNeighboursAlive == 3
 
   "A cell" should {
     "not be alive on next generation if it has less than 2 neighbours alive" in {
-      isAliveNextGen(numberOfNeighboursAlive = 0) shouldBe false
-      isAliveNextGen(numberOfNeighboursAlive = 1) shouldBe false
+      isAliveNextGen(numberOfNeighboursAlive = 0, isAlive = true) shouldBe false
+      isAliveNextGen(numberOfNeighboursAlive = 1, isAlive = true) shouldBe false
     }
   }
 
   "A cell" should {
     "be alive on next generation if it has 2 or 3 neighbours alive" in {
-      isAliveNextGen(numberOfNeighboursAlive = 2) shouldBe true
-      isAliveNextGen(numberOfNeighboursAlive = 3) shouldBe true
+      isAliveNextGen(numberOfNeighboursAlive = 2, isAlive = true) shouldBe true
+      isAliveNextGen(numberOfNeighboursAlive = 3, isAlive = true) shouldBe true
     }
   }
 
