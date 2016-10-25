6985bc208a8afe0250fb109f0a16ac129ce0089b
Carlos <carlospb86@gmail.com>
2016-10-25 20:30:35 +0100

    A cell should be alive on next generation if it has 2 or 3 neighbours alive

--- src/test/scala/org/craftycoder/GameOfLife.scala
+++ src/test/scala/org/craftycoder/GameOfLife.scala
@@ -4,7 +4,7 @@ import org.scalatest.{Matchers, WordSpecLike}
 
 class GameOfLife extends WordSpecLike with Matchers {
 
-  def isAliveNextGen(numberOfNeighboursAlive: Int): Boolean = false
+  def isAliveNextGen(numberOfNeighboursAlive: Int): Boolean = numberOfNeighboursAlive == 2 || numberOfNeighboursAlive == 3
 
   "A cell" should {
     "not be alive on next generation if it has less than 2 neighbours alive" in {
@@ -12,5 +12,13 @@ class GameOfLife extends WordSpecLike with Matchers {
       isAliveNextGen(numberOfNeighboursAlive = 1) shouldBe false
     }
   }
+
+  "A cell" should {
+    "be alive on next generation if it has 2 or 3 neighbours alive" in {
+      isAliveNextGen(numberOfNeighboursAlive = 2) shouldBe true
+      isAliveNextGen(numberOfNeighboursAlive = 3) shouldBe true
+    }
+  }
+
 
 }
