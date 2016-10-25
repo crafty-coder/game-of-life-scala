b59f369b08392d42c4f526884ce86127cd105a41
Carlos <carlospb86@gmail.com>
2016-10-25 21:30:57 +0100

    Refactor: Add type Cell

--- src/test/scala/org/craftycoder/GameOfLife.scala
+++ src/test/scala/org/craftycoder/GameOfLife.scala
@@ -4,7 +4,9 @@ import org.scalatest.{Matchers, WordSpecLike}
 
 class GameOfLife extends WordSpecLike with Matchers {
 
-  def isAliveNextGen(numberOfNeighboursAlive: Int, isAlive: Boolean): Boolean = (numberOfNeighboursAlive, isAlive) match {
+  type Cell = Boolean
+
+  def isAliveNextGen(numberOfNeighboursAlive: Int, cell: Cell): Boolean = (numberOfNeighboursAlive, cell) match {
     case (2, true) => true
     case (3, _) => true
     case _ => false
@@ -12,39 +14,40 @@ class GameOfLife extends WordSpecLike with Matchers {
 
   "Any cell" should {
     "not be alive on next generation if it has less than 2 neighbours alive" in {
-      isAliveNextGen(numberOfNeighboursAlive = 0, isAlive = true) shouldBe false
-      isAliveNextGen(numberOfNeighboursAlive = 1, isAlive = true) shouldBe false
+      isAliveNextGen(numberOfNeighboursAlive = 0, cell = true) shouldBe false
+      isAliveNextGen(numberOfNeighboursAlive = 1, cell = true) shouldBe false
 
-      isAliveNextGen(numberOfNeighboursAlive = 0, isAlive = false) shouldBe false
-      isAliveNextGen(numberOfNeighboursAlive = 1, isAlive = false) shouldBe false
+      isAliveNextGen(numberOfNeighboursAlive = 0, cell = false) shouldBe false
+      isAliveNextGen(numberOfNeighboursAlive = 1, cell = false) shouldBe false
     }
   }
 
   "A cell" should {
     "be alive on next generation if it has 2 or 3 neighbours alive" in {
-      isAliveNextGen(numberOfNeighboursAlive = 2, isAlive = true) shouldBe true
-      isAliveNextGen(numberOfNeighboursAlive = 3, isAlive = true) shouldBe true
+      isAliveNextGen(numberOfNeighboursAlive = 2, cell = true) shouldBe true
+      isAliveNextGen(numberOfNeighboursAlive = 3, cell = true) shouldBe true
     }
   }
 
   "A dead cell" should {
     "not be alive on next generation if it has 2 neighbours alive" in {
-      isAliveNextGen(numberOfNeighboursAlive = 2, isAlive = false) shouldBe false
+      isAliveNextGen(numberOfNeighboursAlive = 2, cell = false) shouldBe false
     }
   }
 
   "Any cell" should {
     "not be alive on next generation if it has more than 3 neighbours alive" in {
-      isAliveNextGen(numberOfNeighboursAlive = 4, isAlive = false) shouldBe false
-      isAliveNextGen(numberOfNeighboursAlive = 4, isAlive = true) shouldBe false
+      isAliveNextGen(numberOfNeighboursAlive = 4, cell = false) shouldBe false
+      isAliveNextGen(numberOfNeighboursAlive = 4, cell = true) shouldBe false
     }
   }
 
   "A dead cell" should {
     "be alive on next generation if it has 3 neighbours alive" in {
-      isAliveNextGen(numberOfNeighboursAlive = 3, isAlive = false) shouldBe true
+      isAliveNextGen(numberOfNeighboursAlive = 3, cell = false) shouldBe true
     }
   }
+
 
 
 }
