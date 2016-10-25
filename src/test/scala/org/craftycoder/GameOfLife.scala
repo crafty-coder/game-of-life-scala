54ea265584fcbc75d02f59de1a1addb9f804e1e8
Carlos <carlospb86@gmail.com>
2016-10-25 22:08:27 +0100

    A 1x1 board which contains a dead cell should stay the same in the next gen

--- src/test/scala/org/craftycoder/GameOfLife.scala
+++ src/test/scala/org/craftycoder/GameOfLife.scala
@@ -6,6 +6,9 @@ class GameOfLife extends WordSpecLike with Matchers {
 
   type Cell = Boolean
   type Position = (Int, Int)
+  type CellBoard = (Cell, Position)
+  type Board = List[CellBoard]
+
 
   def isAliveNextGen(numberOfNeighboursAlive: Int, cell: Cell): Boolean = (numberOfNeighboursAlive, cell) match {
     case (2, true) => true
@@ -23,6 +26,9 @@ class GameOfLife extends WordSpecLike with Matchers {
       case _ => false
     }
   }
+
+  def nextGen(board: Board): Board = board
+
 
   "Any cell" should {
     "not be alive on next generation if it has less than 2 neighbours alive" in {
@@ -96,6 +102,15 @@ class GameOfLife extends WordSpecLike with Matchers {
       areNeighbours((1, 1), (3, 1)) shouldBe false
 
       areNeighbours((1, 1), (3, 3)) shouldBe false
+    }
+  }
+
+  "A 1x1 board which contains a dead cell" should {
+    "stay the same in the next gen" in {
+      val initialBoard = List((false, (0, 0)))
+      val expectedBoard = List((false, (0, 0)))
+
+      nextGen(initialBoard) shouldBe expectedBoard
     }
   }
 }
